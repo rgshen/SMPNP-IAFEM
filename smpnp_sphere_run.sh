dc4f509ed89f0d83ec1640742cd7af4748883a46
rgshen <94184229+rgshen@users.noreply.github.com>
2022-05-14 20:05:11 +0800

    Update smpnp_sphere_run.sh

--- smpnp_sphere_run.sh
+++ smpnp_sphere_run.sh
@@ -1,15 +1,15 @@
 # Molecular sphere simulation
 
 mpirun -np 32 ./PNP $c_bulk $c_bulk \
- -dof_type P1 -options_file ./solver_Axb/advanced1.options \
+ -dof_type P1 -options_file advanced1.options \
  -solute_region 1 \
  -solvent_region 2 \
- -fn_mesh ./mesh/qiaoyu_sphere80.mesh \
- -fn_pqr ./mesh/qiaoyu_sphere80.pqr \
- -fn_bcmap ./mesh/srg_sphere_old.bc_dirichlet \
- -fn_ions ./ions_infor/2ions_HR_SRG.ions \
+ -fn_mesh qiaoyu_sphere80.mesh \
+ -fn_pqr qiaoyu_sphere80.pqr \
+ -fn_bcmap srg_sphere_old.bc_dirichlet \
+ -fn_ions 2ions_HR_SRG.ions \
  -MOD 0 \
- -Stabilize 1 \
+ -Stabilize 3 \
  -tol 1.0e-6 \
  -u_bulk 0.0 \
  -Alpha 0.1 \
@@ -20,12 +20,12 @@ mpirun -np 32 ./PNP $c_bulk $c_bulk \
  #-dof_circle_value_calculation 1 \
  #-out_dof_section_file dof_section_smpnp.out 
  
-#-Stabilize 3 ----IAFEM 方法； -Stabilize 1  SUPG
+#-Stabilize 3 ----IAFEM; -Stabilize 1  SUPG
 
 
-#-MOD 6 -Stabilize 3  IAFEM ---> 为了算smpnp，但可退化成 pnp---所以先验证pnp
+#-MOD 6 -Stabilize 3  smpnp-IAFEM 
 #-MOD 7 --Primitive for smpnp by  Qiaoyu's style
 
-#-MOD 0 -Stabilize 3 原pnp code EAFE  ---作为结果对照 Stabilize 0 --Primitive for pnp
+#-MOD 0 -Stabilize 3  pnp-IAFEM  
 #-MOD 1 -Primitive for smpnp
 
